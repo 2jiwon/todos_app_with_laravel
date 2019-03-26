d8b257b9908821a78f5dc432e989b449c62ae96d
2jiwon <ljwjulian@gmail.com>
2019-03-26 08:11:36 +0000

    튜토리얼 011(Styling views with bootstrap)까지 완료

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,17 +4,49 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <title>Todos app</title>
 </head>
 
 <body>
-    <h1>Todos Page</h1>
+    <div class="container">
 
-    @foreach($todos as $todo)
+      <h1 class="text-center my-5">Todos Page</h1>
+
+       <div class="row justify-content-center">
+
+         <div class="col-md-8">
+            
+            <div class="card card-default">
+              <div class="card-header">
+                 Todos
+              </div>
+              
+              <div class="card-body">
+
+                <ul class="list-group">
+
+                @foreach($todos as $todo)
   
-      <li> {{ $todo->name }} </li> 
+                  <li class="list-group-item">
+
+                    {{ $todo->name }} 
     
-    @endforeach
+                    <button class="btn btn-primary btn-sm float-right">View</button>
+
+                  </li> 
+                
+                @endforeach
+
+                </ul>
+
+              </div>
+            </div>  
+
+         </div>
+       </div>
+
+    </div>  
 	
 </body>
 
