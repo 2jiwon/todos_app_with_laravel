7e0b56e2ef3b1757c9ddfd6db7f50296de78bf70
2jiwon <ljwjulian@gmail.com>
2019-03-27 04:32:54 +0000

    튜토리얼 014(Introduction to layout files)까지 완료

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>Todos app</title>
-</head>
+@extends('layouts.app')
 
-<body>
-    <div class="container">
+@section('title')
 
+Todos List
+
+@endsection
+
+
+@section('content')
       <h1 class="text-center my-5">Todos Page</h1>
 
        <div class="row justify-content-center">
-
          <div class="col-md-8">
             
             <div class="card card-default">
@@ -34,7 +30,6 @@
     
                     <!-- <button class="btn btn-primary btn-sm float-right">View</button> -->
                     <a class="btn btn-primary btn-sm float-right" href="/todos/{{ $todo->id }}">View</a>
-                  
 
                   </li> 
                 
@@ -47,9 +42,4 @@
 
          </div>
        </div>
-
-    </div>  
-	
-</body>
-
-</html>
+@endsection
