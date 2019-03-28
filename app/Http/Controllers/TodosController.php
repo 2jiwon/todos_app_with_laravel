f6f64b900f47200ffa0e9ecb5fc924015896d485
2jiwon <ljwjulian@gmail.com>
2019-03-28 01:51:05 +0000

    튜토리얼 016(Validating data)까지 완료

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -29,8 +29,11 @@ class TodosController extends Controller
 
     public function store()
     {
-      //dd(request()->all());
-      
+      $this->validate(request(), [
+        'name' => 'required|min:6|max:12',
+        'description' => 'required' 
+      ]);
+
       $data = request()->all();
 
       $todo = new Todo();
